35fc5a9f0516208ae506ba1fa61ef9a9db22c1d6
Aaron Graupman <aaron.emmi3@gmail.com>
2017-05-22 16:38:16 -0600

    expand slots

--- slots.rb
+++ slots.rb
@@ -1,16 +1,30 @@
+require 'pry'
+
+require_relative 'check_input'
+
 @x = 1 + rand(55)
 @spin_1
 @spin_2
 @spin_3
 
+def initialize(player) 
+end
+
 def slot_menu
-  puts '\nWelcome to Slots'
-  puts 'Press "p" to play for $1'
+  puts "\nWelcome to Slots"
+  puts 'Press "p" to play for $1.'
   puts 'Press "w" to view your wallet.'
   puts 'Press "s" to switch profiles.'
   puts 'Press "m" to return to the main menu.'
-  puts 'Press "q" to leave the casino'
+  puts 'Press "q" to leave the casino.'
+  puts
 end
 
 while true
   slot_menu
+  sm_input = gets.strip.downcase
+when 'p'
+  play_slot
+else
+  CheckInput.new(player)
+end
