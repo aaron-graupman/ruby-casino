5efb89db5f3639e6b943995f6433b73ee1e8b022
Aaron Graupman <aaron.emmi3@gmail.com>
2017-05-22 12:09:57 -0600

    update heads/tails

--- heads_tails.rb
+++ heads_tails.rb
@@ -1,6 +1,26 @@
+require_relative 'check_input'
+
+
 class HeadsTails
   def initialize(player)
-    puts ''
-    puts '#{player.name} you have a balance of #{player.wallet.amount}'
+    puts '\nWelcome to Heads or Tails\n'
+    puts '#{player.name} you have a balance of #{player.wallet.amount}\n'
   end
 end
+
+def coin_toss_menu
+  puts 'Press 1 to make a bet.'
+  puts 'Press 2 to return to the main menu.'
+  puts 'Press 3 to leave the casino.\n'
+end
+
+while true
+  coin_toss_menu
+  ctm_input = gets.to_i
+  puts
+    case ctm_input
+      when 1
+        puts 'Heads or Tails?\n'
+        coin_toss
+      else
+        CheckInput.new(ctm_input)
