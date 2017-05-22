e964bf90fd555e14483105d5126cc74cb3f8cfde
Branden Bird <brandenjbird@gmail.com>
2017-05-22 11:48:46 -0600

    Add RoulletteSpin class

---
+++ routlette_spin.rb
@@ -0,0 +1,29 @@
+require "pry"
+
+class RoulletteSpin
+  attr_accessor :spin, :color
+  def initialize
+   spin
+   color
+  end
+
+  def spin
+   @spin = rand(12)
+  end
+
+  def color
+    case @spin
+    when 1,3,5,7,9,12
+     @color = "Red"
+    when 2,4,6,8,10,11
+     @color = "Black"
+    when 0
+     @color = "Green"
+    end
+  end
+
+  def show_dice
+    print "Spin: ", @spin
+  end
+
+end
