7fde742a19eedbf5091432180d04e48cae85cdeb
Aaron Graupman <aaron.emmi3@gmail.com>
2017-05-22 13:10:31 -0600

    update heads/tails

--- heads_tails.rb
+++ heads_tails.rb
@@ -1,18 +1,37 @@
 require_relative 'check_input'
-
+@ct = ['heads', 'tails']
+@ctb_input = 0.0
 
 class HeadsTails
   def initialize(player)
     puts '\nWelcome to Heads or Tails\n'
-    puts '#{player.name} you have a balance of #{player.wallet.amount}\n'
+    puts '#{player.name}, you have a balance of #{player.wallet.amount}\n'
   end
 end
 
 def coin_toss_menu
-  puts 'Press 1 to make a bet.'
+  puts 'Press 1 to play. A winning bet pays 1 to 1.'
   puts 'Press 2 to return to the main menu.'
   puts 'Press 3 to leave the casino.\n'
 end
+
+def coin_toss
+  puts '\nHeads or Tails?'
+  ct_input = gets.strip.downcase
+    if ct_input = 'heads' || 'tails'
+    else puts '\nInvalid selection, try again when you are sober.'
+      coin_toss
+    end
+    ct_result = @ct.sample
+    puts "\nIt's" ct_result
+    if ct_input = ct_result
+      then
+      puts '\nYou win'
+      ct_win
+    else
+      puts '\nYou lose sucker!'
+      coin_toss_menu
+    end
 
 while true
   coin_toss_menu
@@ -20,7 +39,10 @@ while true
   puts
     case ctm_input
       when 1
-        puts 'Heads or Tails?\n'
-        coin_toss
+        puts 'How much money would you like to bet (Ex. 5.50)?\n'
+        @ctb_input = gets.to_f
+        coin_toss_bet
       else
         CheckInput.new(ctm_input)
+    end
+end
